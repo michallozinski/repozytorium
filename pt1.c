9337f0c15e2e490d89f35a66bb0a24cb91ce2970
jszymm <joannaszymanek95@gmail.com>
2016-04-20 09:00:11 +0200

    message

---
+++ pt1.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+
+ int main ()
+ {
+   int a, b;
+   a = 4;
+   b = 6;
+   if (a==b) {
+     printf ("a jest równe b\n");
+   } else {
+     printf ("a nie jest równe b\n");
+   }
+   return 0;
+ }
